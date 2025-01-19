dc58f1368d26e46c4f536771f2b0e1111e6141d9
Gaurav <gauravkumarop3@gmail.com>
2025-01-20 00:59:17 +0530

    Adding multiplayer mode

---
+++ Client/src/App.tsx
@@ -0,0 +1,28 @@
+import Home from "./Pages/Home";
+import Instruction from "./Pages/Instruction";
+import Question from "./Pages/Question";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Submit from "./Pages/Submit";
+import Error from "./Components/Error";
+import Multiplayer from "./Pages/multiplayer/Multiplayer";
+import MultiSubmit from "./Pages/multiplayer/MultiSubmit";
+// import Lobby from "./Pages/Lobby";
+
+function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/instruction", element: <Instruction /> },
+    { path: "/instruction/question", element: <Question /> },
+    { path: "/instruction/question/submit", element: <Submit /> },
+    { path: "/multiplayer", element: <Multiplayer /> },
+    { path: "/multiplayer/multisubmit", element: <MultiSubmit /> },
+    { path: "*", element: <Error /> },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
