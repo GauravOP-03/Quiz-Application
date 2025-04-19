0f3a0e1500b3269251b51517ec9da477c4aec9c7
Gaurav <gauravkumarop3@gmail.com>
2025-04-19 16:47:49 +0530

    adding caching to node and some comments

--- Server/index.js
+++ Server/index.js
@@ -5,10 +5,12 @@ const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const axios = require("axios");
+const NodeCache = require("node-cache");
 
+// configuring cors
 app.use(cors());
 const server = createServer(app);
-const allowedOrigins = [process.env.origin1, process.env.origin2];
+const allowedOrigins = [process.env.origin1, process.env.origin2]; //setting origin
 const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
@@ -16,11 +18,24 @@ const io = new Server(server, {
   },
 });
 
+// configuring node-cache
+const questionCache = new NodeCache({ stdTTL: 600 }); //stdTTL:600 - delete after 10 min
 async function getQuestion(question, category) {
+  const cacheKey = `${category}-${question}`;
+  const cached = questionCache.get(cacheKey);
+  // if same category and number of ques. exist then return cache data
+  if (cached) {
+    console.log("serving from cache");
+    // console.log(cached);
+    return cached;
+  }
+  // else return api data
   const allQuestion = await axios.get(
     `https://opentdb.com/api.php?amount=${question}&category=${category}&type=multiple`
   );
   // console.log(allQuestion.data);
+  questionCache.set(cacheKey, allQuestion.data);
+  console.log("Serving from API");
   return allQuestion.data;
 }
 
@@ -28,12 +43,13 @@ async function getQuestion(question, category) {
 let rooms = {}; // { roomId: [players] }
 let score = {};
 
+// initial connection
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
   // Handle host details
   socket.on("host_details", (data) => {
-    console.log("Host details received:", data);
+    // console.log("Host details received:", data);
     const id = socket.id;
     const newHost = { ...data.hostInput, id, role: "Host" };
     socket.emit("get_id", socket.id);
@@ -45,7 +61,7 @@ io.on("connection", (socket) => {
 
       // Add the host to the specified room
       socket.join(roomId);
-      console.log(rooms);
+      // console.log(rooms);
     } else {
       socket.emit("room_error", "room is already created");
     }
@@ -56,14 +72,14 @@ io.on("connection", (socket) => {
 
   // Handle player details
   socket.on("player_details", (data) => {
-    console.log("Player details received:", data);
+    // console.log("Player details received:", data);
     const id = socket.id;
     const newPlayer = { ...data.playerInput, id, role: "Player" };
     socket.emit("get_id", socket.id);
     // Add the player to the room's player list
     const roomId = data.playerInput.roomId;
     if (score[roomId]) {
-      console.log(score[roomId]);
+      // console.log(score[roomId]);
       socket.emit("room_error", "Game is already started");
     } else if (rooms[roomId]) {
       if (rooms[roomId].length >= 4) {
@@ -81,6 +97,7 @@ io.on("connection", (socket) => {
     }
   });
 
+  // trigger when host start quiz
   socket.on("start_quiz", (data) => {
     // console.log(data);
     getQuestion(data.question, data.category)
@@ -94,8 +111,9 @@ io.on("connection", (socket) => {
       });
   });
 
+  // trigger when any player score
   socket.on("set_score", (data) => {
-    console.log(data);
+    // console.log(data);
     const roomId = data.room;
     if (!score[roomId]) {
       score[roomId] = [];
@@ -116,10 +134,10 @@ io.on("connection", (socket) => {
 
   // deleting players
   socket.on("delete_players", (data) => {
-    console.log(data);
+    // console.log(data);
     // console.log(rooms[data.roomId]);
     const players = rooms[data.roomId].filter((d) => d.id !== data.id);
-    console.log(players);
+    // console.log(players);
     rooms[data.roomId] = players;
     io.to(data.roomId).emit("connected_players", rooms[data.roomId]);
     const target = io.sockets.sockets.get(data.id);
